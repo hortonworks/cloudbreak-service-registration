94d4898996bff50e2a67160b5007c6fc9b0f77c2
Krisztian Horvath <krisztian.horvath@sequenceiq.com>
2017-01-13 16:51:26 +0100

    wait for cluster to be posted

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -14,6 +17,8 @@ const (
 	DEFAULT_AMBARI_CREDENTIALS_PATH = "/srv/pillar/ambari/credentials.sls"
 	DEFAULT_AMBARI_SERVER_PATH      = "/srv/pillar/ambari/server.sls"
 	SLEEP_TIME                      = 5
+	AMBARI_REQUEST_BY_HEADER        = "X-Requested-By"
+	AMBARI_API_URL                  = "/api/v1"
 )
 
 type Ambari struct {
@@ -22,6 +27,14 @@ type Ambari struct {
 		Username string `yaml:"username"`
 		Password string `yaml:"password"`
 	} `yaml:"ambari"`
+}
+
+type ClusterResponse struct {
+	Items []struct {
+		Cluster struct {
+			Name string `json:"cluster_name"`
+		} `json:"Clusters"`
+	} `json:"items"`
 }
 
 func main() {
@@ -37,6 +50,7 @@ func main() {
 	waitFile(serverPath)
 	ambari := readCredentials(credentialsPath)
 	ambari.Config.Address = readServer(serverPath).Config.Address
+	waitForCluster(ambari)
 }
 
 func waitFile(path string) {
@@ -92,4 +106,29 @@ func readServer(path string) *Ambari {
 		}
 	}
 	return ambari
+}
+
+func waitForCluster(ambari *Ambari) string {
+	req, _ := http.NewRequest("GET", "http://"+ambari.Config.Address+":8080"+AMBARI_API_URL+"/clusters", nil)
+	req.Header.Add(AMBARI_REQUEST_BY_HEADER, "ambari")
+	req.SetBasicAuth(ambari.Config.Username, ambari.Config.Password)
+	httpClient := &http.Client{}
+	var clusterName string
+	for len(clusterName) == 0 {
+		resp, err := httpClient.Do(req)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		body, _ := ioutil.ReadAll(resp.Body)
+		log.Println("Clusters resonse: " + string(body))
+		var cresp ClusterResponse
+		decoder := json.NewDecoder(strings.NewReader(string(body)))
+		decoder.Decode(&cresp)
+		if len(cresp.Items[0].Cluster.Name) > 0 {
+			clusterName = cresp.Items[0].Cluster.Name
+		}
+	}
+	log.Println("Found cluster: " + clusterName)
+	return clusterName
 }
